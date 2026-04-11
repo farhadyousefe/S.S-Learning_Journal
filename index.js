9fa8cb684917526ab99ee668f1056005a6632f9a
Farhad Yousefe <farhadyousefe@gmail.com>
2026-04-11 20:17:12 +0430

    feat(showingAboutMe function): add function and eventlistener to handle about-me btn and render about me section

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 // Java Script for the Journey website
 const hero = document.getElementById("hero_section");
 const journey = document.getElementById("journey_section");
+const aboutMe = document.getElementById("about_me");
 
 const journeyBtn = document.getElementById("my_journey_btn");
+const aboutMeBtn = document.getElementById("about_me_btn");
 
 journeyBtn.addEventListener("click", showingJourneySection);
 
 function showingJourneySection() {
   journey.classList.remove("display");
   hero.classList.add("display");
+}
+
+aboutMeBtn.addEventListener("click", (event) => {
+  console.log("hello Afghanistan");
+  event.preventDefault();
+  showingAboutMeSection();
+});
+function showingAboutMeSection() {
+  if (!journey.classList.contains("display")) {
+    journey.classList.add("display");
+  }
+  hero.classList.add("display");
+  aboutMe.classList.remove("display");
 }
