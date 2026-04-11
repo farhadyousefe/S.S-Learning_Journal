ac4db9eabe850e6bf75beda41709abc15c732eef
Farhad Yousefe <farhadyousefe@gmail.com>
2026-04-11 19:33:14 +0430

    feat(showing journey section): added function to show journey section when journey headline is clicked

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Java Script for the Journey website
+const hero = document.getElementById("hero_section");
+const journey = document.getElementById("journey_section");
+
+const journeyBtn = document.getElementById("my_journey_btn");
+
+journeyBtn.addEventListener("click", showingJourneySection);
+
+function showingJourneySection() {
+  journey.classList.remove("display");
+  hero.classList.add("display");
+}
